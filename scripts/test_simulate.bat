d91eb1690fb9ff76cc3eafe6f3ed268ba8223f76
Caner Göksel Sonuzun <caner@cgsdev.io>
2025-08-05 01:28:15 +0300

    escape characters

--- scripts/test_simulate.bat
+++ scripts/test_simulate.bat
@@ -4,6 +4,6 @@ set WORKSPACE=%cd%\test_workspace
 set LOGCONFIG=%cd%\log4j.xml
 
 echo === Simulating LC3 Test Run ===
-echo "eclipseC -launcher.ini logiCAD3.ini -application com.logicals.application.importer.application -noSplash -projectPath \"%PROJECTPATH%\" -data \"%WORKSPACE%\" -vmargs -Dlog4j.configuration=file:%LOGCONFIG%"
+echo eclipseC -launcher.ini logiCAD3.ini ^-application com.logicals.application.importer.application ^-noSplash ^-projectPath %PROJECTPATH% ^-data %WORKSPACE% ^-vmargs -Dlog4j.configuration=file:%LOGCONFIG%
 timeout /t 2 >nul
 echo Tests simulated successfully.
